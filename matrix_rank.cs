9bf1873df272b5eb9b9b7cea6a39b2289dfa407d
mahdiaminik <44305826+mahdiaminik@users.noreply.github.com>
2019-07-08 00:12:07 +0430

    Add files via upload

---
+++ matrix_rank.cs
@@ -0,0 +1,62 @@
+using System;
+namespace MahdiAmini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int size = 5;
+			var A = new double[size, size];
+			
+			//fill matix with random numbers :
+            var rand = new Random();
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    A[i, j] = rand.NextDouble();
+
+            
+
+            // Operation :
+            for (int j = 0; j<size; j++)
+            {
+                for(int i = j+1; i<size; i++)
+                {
+                    if(A[i,j] !=0)
+                    {
+                        if (A[j, j] == 0)
+                        {
+                            //swap Row i , Row j
+                            for (int k = 0; k < size; k++)
+                            {
+                                var temp = A[i, k];
+                                A[i, k] = A[j, k];
+                                A[j, k] = temp;
+                            }
+                        }
+                        double scale = A[i, j] / A[j, j];
+                        for (int k = 0; k < size; k++)
+                            A[i, k] -= scale * A[j, k];
+                    }
+                }
+
+            }
+
+
+            //rank:
+            int rank = size;
+            for (int i = 0; i < size; i++)
+                if (A[i, i] == 0)
+                    rank--;
+            
+         
+            //print rank:
+            Console.WriteLine();
+            Console.WriteLine($"rank is {rank}");
+            Console.ReadKey();
+            
+
+        }
+
+       
+    }
+}
